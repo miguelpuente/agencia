015c2bad2393e075afa0f15c7ba737d48e58d835
Miguel Puente <wilo1985@hotmail.com>
2023-12-04 21:21:50 -0300

    Crud marca y marcamodelo

--- apps/agencia/urls.py
+++ apps/agencia/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import (
-    ModeloListView, ModeloDetailView, ModeloCreateView, ModeloUpdateView, ModeloDeleteView
+    ModeloListView, ModeloDetailView, ModeloCreateView, ModeloUpdateView, ModeloDeleteView,
+    MarcaListView, MarcaDetailView, MarcaCreateView, MarcaUpdateView, MarcaDeleteView,
+    MarcaModeloListView, MarcaModeloDetailView, MarcaModeloCreateView, MarcaModeloUpdateView, MarcaModeloDeleteView,
     )
 
 urlpatterns = [
@@ -10,4 +12,23 @@ urlpatterns = [
     path('modelo/create/', ModeloCreateView.as_view(), name='modelo_create'),
     path('modelo/<int:pk>/update/', ModeloUpdateView.as_view(), name='modelo_update'),
     path('modelo/<int:pk>/delete/', ModeloDeleteView.as_view(), name='modelo_delete'),
+
+    # Rutas para el modelo Marca
+    path('marca/list/', MarcaListView.as_view(), name='marca_list'),
+    path('marca/<int:pk>/', MarcaDetailView.as_view(), name='marca_detail'),
+    path('marca/create/', MarcaCreateView.as_view(), name='marca_create'),
+    path('marca/<int:pk>/update/', MarcaUpdateView.as_view(), name='marca_update'),
+    path('marca/<int:pk>/delete/', MarcaDeleteView.as_view(), name='marca_delete'),
+
+    # Rutas para el modelo MarcaModelo
+    path('marca-modelo/list/', MarcaModeloListView.as_view(),
+         name='marca_modelo_list'),
+    path('marca-modelo/<int:pk>/', MarcaModeloDetailView.as_view(),
+         name='marca_modelo_detail'),
+    path('marca-modelo/create/', MarcaModeloCreateView.as_view(),
+         name='marca_modelo_create'),
+    path('marca-modelo/<int:pk>/update/',
+         MarcaModeloUpdateView.as_view(), name='marca_modelo_update'),
+    path('marca-modelo/<int:pk>/delete/',
+         MarcaModeloDeleteView.as_view(), name='marca_modelo_delete'),
 ]
--- apps/agencia/views.py
+++ apps/agencia/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Modelo
+from .models import Modelo, Marca, MarcaModelo, Auto
 
-
+# CRUD Modelo
 class ModeloListView(ListView):
     model = Modelo
     template_name = 'agencia/modelos/modelo_list.html'
@@ -34,8 +34,74 @@ class ModeloDeleteView(DeleteView):
     success_url = reverse_lazy('modelo_list')
 
 
+# CRUD Marca
+
+class MarcaListView(ListView):
+    model = Marca
+    template_name = 'agencia/marcas/marca_list.html'
+    context_object_name = 'marcas'
 
 
+class MarcaDetailView(DetailView):
+    model = Marca
+    template_name = 'agencia/marcas/marca_detail.html'
+    context_object_name = 'marca'
 
 
+class MarcaCreateView(CreateView):
+    model = Marca
+    template_name = 'agencia/marcas/marca_form.html'
+    context_object_name = 'marca'
+    fields = '__all__'
+    success_url = reverse_lazy('marca_list')
+
+
+class MarcaUpdateView(UpdateView):
+    model = Marca
+    template_name = 'agencia/marcas/marca_form.html'
+    context_object_name = 'marca'
+    fields = '__all__'
+    success_url = reverse_lazy('marca_list')
+
+
+class MarcaDeleteView(DeleteView):
+    model = Marca
+    template_name = 'agencia/marcas/marca_confirm_delete.html'
+    context_object_name = 'marca'
+    success_url = reverse_lazy('marca_list')
+
+
+# CRUD MarcaModelo
+
+class MarcaModeloListView(ListView):
+    model = MarcaModelo
+    template_name = 'agencia/marcasmodelos/marca_modelo_list.html'
+    context_object_name = 'marcasmodelos'
+
+
+class MarcaModeloDetailView(DetailView):
+    model = MarcaModelo
+    template_name = 'agencia/marcasmodelos/marca_modelo_detail.html'
+    context_object_name = 'marcamodelo'
+
+
+class MarcaModeloCreateView(CreateView):
+    model = MarcaModelo
+    template_name = 'agencia/marcasmodelos/marca_modelo_form.html'
+    context_object_name = 'marcamodelo'
+    fields = '__all__'
+    success_url = reverse_lazy('marca_modelo_list')
+
+
+class MarcaModeloUpdateView(UpdateView):
+    model = MarcaModelo
+    template_name = 'agencia/marcasmodelos/marca_modelo_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('marca_modelo_list')
+
+
+class MarcaModeloDeleteView(DeleteView):
+    model = MarcaModelo
+    template_name = 'agencia/marcasmodelos/marca_modelo_confirm_delete.html'
+    success_url = reverse_lazy('marca_modelo_list')
 
